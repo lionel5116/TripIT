5b57c5402f84b09f68c19c5ee46c65aae4115c0e
lioneljones <lioneljones5116@gmail.com>
2022-08-14 15:44:14 -0500

    Initial Commit

---
+++ TripIT/TripITApp.swift
@@ -0,0 +1,17 @@
+//
+//  TripITApp.swift
+//  TripIT
+//
+//  Created by lionel jones on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct TripITApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
