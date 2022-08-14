349854a12e15e3e091a1a32f9d32f6d089f35711
lioneljones <lioneljones5116@gmail.com>
2022-08-14 16:28:34 -0500

    First Commit - Login Screen

--- TripIT/ContentView.swift
+++ TripIT/ContentView.swift
@@ -8,9 +8,74 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var username = ""
+    @State private var password = ""
+    @State private var wrongUsername = 0
+    @State private var wrongPassword = 0
+    @State private var showingLoginScreen = false
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            ZStack {
+                Color.gray
+                    .ignoresSafeArea()
+                Circle()
+                    .scale(1.7)
+                    .foregroundColor(.white.opacity(0.15))
+                Circle()
+                    .scale(1.35)
+                    .foregroundColor(.white)
+                VStack {
+                    Text("Login")
+                        .font(.largeTitle)
+                        .bold()
+                        .padding()
+                    TextField("Username", text: $username)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongUsername))
+                    
+                    SecureField("Password", text: $password)
+                        .padding()
+                        .frame(width: 300, height: 50)
+                        .background(Color.black.opacity(0.05))
+                        .cornerRadius(10)
+                        .border(.red, width: CGFloat(wrongPassword))
+                    
+                    Button("Login") {
+                        authenticateUser(username: username, password: password)
+                    }
+                    .foregroundColor(.white)
+                    .frame(width: 300, height: 50)
+                    .background(Color.blue)
+                    .cornerRadius(10)
+                    
+                    NavigationLink(destination: Text("You are logged in \(username)"),isActive: $showingLoginScreen) {
+                        EmptyView();
+                    }
+                           
+                }//VStack
+            }  //ZStack
+            .navigationBarHidden(true)
+        } //NavigationView
+    }//View (body)
+    
+    func authenticateUser(username: String, password: String) {
+        //your own database and logic will go here
+        if username.lowercased() == "lionel5116" {
+            wrongUsername = 0
+            if password.lowercased() == "5116" {
+                wrongPassword = 0
+                showingLoginScreen = true
+            }
+            else {
+                wrongPassword = 2
+            }
+        } else {
+            wrongPassword = 2
+        }
     }
 }
 
